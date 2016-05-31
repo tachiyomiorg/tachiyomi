5ff1a27008b7f9fddafa51c94b5a7f55a49a2823
len <len@kanade.eu>
2016-05-31 17:53:15 +0200

    Backend support for Anilist

---
+++ app/src/main/java/eu/kanade/tachiyomi/data/mangasync/anilist/model/OAuth.kt
@@ -0,0 +1,8 @@
+package eu.kanade.tachiyomi.data.mangasync.anilist.model
+
+data class OAuth(
+        val access_token: String,
+        val token_type: String,
+        val expires: Long,
+        val expires_in: Long,
+        val refresh_token: String?)
