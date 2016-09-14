9dac958ed1414338dfff825c8d7dbe78fb98209a
nulldev <contactnulldev@gmail.com>
2016-09-14 16:57:01 -0400

    Fixed problems found by inorichi.

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/sync/LibrarySyncPresenter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/sync/LibrarySyncPresenter.kt
@@ -11,7 +11,6 @@ import rx.Observable
 import rx.functions.Func1
 import rx.schedulers.Schedulers
 import rx.subjects.PublishSubject
-import timber.log.Timber
 import uy.kohesive.injekt.injectLazy
 import xyz.nulldev.ts.sync.model.SyncResult
 
@@ -68,42 +67,43 @@ class LibrarySyncPresenter(): BasePresenter<LibrarySyncDialogFragment>() {
             //Sync library
             syncClient.syncLibrariesWithProgress(it.first,
                     it.second).concatMap<LibrarySyncProgressEvent> (Func1<SyncResult, Observable<LibrarySyncProgressEvent>> { result ->
-                if(result is SyncResult.Progress) {
+                when(result) {
                     //Update progress
-                    Observable.just(LibrarySyncProgressEvent.ProgressUpdated(result.status))
-                } else if (result is SyncResult.Success) {
-                    Observable.create { sub ->
+                    is SyncResult.Progress -> Observable.just(LibrarySyncProgressEvent.ProgressUpdated(result.status))
+                    is SyncResult.Success -> {
                         //Report to user we are saving changes (it might take a while)
-                        sub.onNext(LibrarySyncProgressEvent.ProgressUpdated(context.getString(R.string.saving_changes)))
-                        //Everything in a transaction to ensure we don't delete the user's entire library in an error
-                        db.inTransaction {
-                            //Delete old mangas
-                            val oldMangas = db.getMangas().executeAsBlocking()
-                            if (oldMangas.size > 0) {
-                                db.deleteMangas(oldMangas).executeAsBlocking()
-                                db.deleteOldMangasCategories(oldMangas).executeAsBlocking()
+                        val savedObservable = Observable.just(LibrarySyncProgressEvent.ProgressUpdated(context.getString(R.string.saving_changes)))
+                        val completedObservable = Observable.fromCallable {
+                            //Everything in a transaction to ensure we don't delete the user's entire library in an error
+                            db.inTransaction {
+                                //Delete old mangas
+                                val oldMangas = db.getMangas().executeAsBlocking()
+                                if (oldMangas.size > 0) {
+                                    db.deleteMangas(oldMangas).executeAsBlocking()
+                                    db.deleteOldMangasCategories(oldMangas).executeAsBlocking()
+                                }
+                                //Delete old categories
+                                val oldCategories = db.getCategories().executeAsBlocking()
+                                if (oldCategories.size > 0) {
+                                    db.deleteCategories(oldCategories).executeAsBlocking()
+                                }
+                                //Write new library to DB
+                                backupManager.restoreFromJson(JsonParser().parse(result.serializedLibrary).asJsonObject)
+                                //Save new library state
+                                syncManager.saveLastLibraryState(result.serializedLibrary)
                             }
-                            //Delete old categories
-                            val oldCategories = db.getCategories().executeAsBlocking()
-                            if (oldCategories.size > 0) {
-                                db.deleteCategories(oldCategories).executeAsBlocking()
-                            }
-                            //Write new library to DB
-                            backupManager.restoreFromJson(JsonParser().parse(result.serializedLibrary).asJsonObject)
-                            //Save new library state
-                            syncManager.saveLastLibraryState(result.serializedLibrary)
+                            //Sync was successful so reset last failed sync count
+                            syncManager.lastFailedSyncCount = 0
+                            //Sync ok, report conflicts
+                            LibrarySyncProgressEvent.Completed(result.conflicts)
                         }
-                        //Sync ok, report conflicts
-                        sub.onNext(LibrarySyncProgressEvent.Completed(result.conflicts))
-                        //Sync was successful so reset last failed sync count
-                        syncManager.lastFailedSyncCount = 0
+                        Observable.merge(savedObservable, completedObservable)
                     }
-                } else if(result is SyncResult.Fail) {
-                    //Log this failed sync
-                    syncManager.lastFailedSyncCount++
-                    Observable.just<LibrarySyncProgressEvent>(LibrarySyncProgressEvent.Error(result.error ?: context.getString(R.string.sync_unknown_error)))
-                } else {
-                    throw NotImplementedError("Unknown SyncResult type!")
+                    is SyncResult.Fail -> {
+                        //Log this failed sync
+                        syncManager.lastFailedSyncCount++
+                        Observable.just<LibrarySyncProgressEvent>(LibrarySyncProgressEvent.Error(result.error ?: context.getString(R.string.sync_unknown_error)))
+                    }
                 }
             })
         }.subscribeOn(Schedulers.io()).subscribe(syncProgressSubject)
@@ -115,11 +115,11 @@ class LibrarySyncPresenter(): BasePresenter<LibrarySyncDialogFragment>() {
      * @returns Pair<OLD, NEW>
      */
     fun getLibraryObservable():Observable<Pair<String, String>> {
-        return Observable.create {
+        return Observable.fromCallable {
             //Get libraries
             val oldLibrary = syncManager.getLastLibraryState()
             val newLibrary = backupManager.backupToJson(favoritesOnly = true).toString()
-            it.onNext(Pair(oldLibrary, newLibrary))
+            Pair(oldLibrary, newLibrary)
         }
     }
 }
