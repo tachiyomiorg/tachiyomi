cc70888d2f96ebbba180bc786bdd0c19fe98d643
Gianmarco David <blackgins@gmail.com>
2016-12-28 19:45:02 +0100

    Added circular thumbnails to the catalogue list view, like the ones in the library list view

--- app/src/main/java/eu/kanade/tachiyomi/ui/catalogue/CatalogueListHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/catalogue/CatalogueListHolder.kt
@@ -1,6 +1,8 @@
 package eu.kanade.tachiyomi.ui.catalogue
 
 import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import eu.kanade.tachiyomi.data.database.models.Manga
 import eu.kanade.tachiyomi.util.getResourceColor
 import kotlinx.android.synthetic.main.item_catalogue_list.view.*
@@ -29,5 +31,28 @@ class CatalogueListHolder(private val view: View, adapter: CatalogueAdapter, lis
     override fun onSetValues(manga: Manga) {
         view.title.text = manga.title
         view.title.setTextColor(if (manga.favorite) favoriteColor else unfavoriteColor)
+
+        setImage(manga)
     }
+
+    /**
+     * Updates the image for this holder. Useful to update the image when the manga is initialized
+     * and the url is now known.
+     *
+     * @param manga the manga to bind.
+     */
+    fun setImage(manga: Manga) {
+        Glide.clear(view.thumbnail)
+        if (!manga.thumbnail_url.isNullOrEmpty()) {
+            Glide.with(view.context)
+                    .load(manga)
+                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                    .centerCrop()
+                    .dontAnimate()
+                    .skipMemoryCache(true)
+                    .placeholder(android.R.color.transparent)
+                    .into(view.thumbnail)
+        }
+    }
+
 }
