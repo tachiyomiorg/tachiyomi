9926ba6161ed0a3707eb989b016dfc68bd23dfb8
Carlos Escobedo <cargo8005@gmail.com>
2017-05-08 14:41:29 -0400

    fixed excess blank line

--- app/src/main/java/eu/kanade/tachiyomi/data/database/models/ChapterImpl.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/database/models/ChapterImpl.kt
@@ -26,9 +26,6 @@ class ChapterImpl : Chapter {
 
     override var source_order: Int = 0
 
-
-
-
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || javaClass != other.javaClass) return false
