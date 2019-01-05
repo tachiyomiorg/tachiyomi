1ed1ae9a677e4753620a3d8b4c2bb31ef9af3827
Carlos Escobedo <cargo8005@gmail.com>
2019-01-05 13:32:08 -0500

    add option to skip chapters marked read

--- app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferenceKeys.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferenceKeys.kt
@@ -103,6 +103,8 @@ object PreferenceKeys {
 
     const val defaultCategory = "default_category"
 
+    const val skipRead = "skip_read"
+
     const val downloadBadge = "display_download_badge"
 
     @Deprecated("Use the preferences of the source")
