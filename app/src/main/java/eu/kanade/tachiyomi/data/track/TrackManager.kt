1b31bae6ba2c58a838309d483bd63e0af5704280
Pavel Mosein <p.mosein@edadeal.ru>
2019-03-19 00:04:12 +0300

    Add shikomori track

--- app/src/main/java/eu/kanade/tachiyomi/data/track/TrackManager.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/TrackManager.kt
@@ -3,23 +3,27 @@ package eu.kanade.tachiyomi.data.track
 import android.content.Context
 import eu.kanade.tachiyomi.data.track.anilist.Anilist
 import eu.kanade.tachiyomi.data.track.kitsu.Kitsu
-import eu.kanade.tachiyomi.data.track.myanimelist.Myanimelist
+import eu.kanade.tachiyomi.data.track.myanimelist.yanimelist
+import eu.kanade.tachiyomi.data.track.shikomori.Shikomori
 
-class TrackManager(private val context: Context) {
+class Trackanager(private val context: Context) {
 
     companion object {
-        const val MYANIMELIST = 1
+        const val YANIELIST = 1
         const val ANILIST = 2
         const val KITSU = 3
+        const val SHIKOORI = 4
     }
 
-    val myAnimeList = Myanimelist(context, MYANIMELIST)
+    val myAnimeList = yanimelist(context, YANIELIST)
 
     val aniList = Anilist(context, ANILIST)
 
     val kitsu = Kitsu(context, KITSU)
 
-    val services = listOf(myAnimeList, aniList, kitsu)
+    val shikomori = Shikomori(context, SHIKOORI)
+
+    val services = listOf(myAnimeList, aniList, kitsu, shikomori)
 
     fun getService(id: Int) = services.find { it.id == id }
 
