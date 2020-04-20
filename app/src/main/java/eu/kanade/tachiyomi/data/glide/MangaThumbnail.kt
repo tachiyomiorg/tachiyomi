1000274a09dd2455b5cf64e051bec953f05e06d1
MCAxiaz <mcaxiaz@gmail.com>
2020-04-20 00:24:37 -0700

    use data class for MangaThumbnail

--- app/src/main/java/eu/kanade/tachiyomi/data/glide/MangaThumbnail.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/glide/MangaThumbnail.kt
@@ -2,27 +2,6 @@ package eu.kanade.tachiyomi.data.glide
 
 import eu.kanade.tachiyomi.data.database.models.Manga
 
-class MangaThumbnail(val manga: Manga) {
+data class MangaThumbnail(val manga: Manga, val url: String?)
 
-    val url = manga.thumbnail_url
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as MangaThumbnail
-
-        if (manga != other.manga) return false
-        if (url != other.url) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = manga.hashCode()
-        result = 31 * result + (url?.hashCode() ?: 0)
-        return result
-    }
-}
-
-fun Manga.toMangaThumbnail() = MangaThumbnail(this)
+fun Manga.toMangaThumbnail() = MangaThumbnail(this, this.thumbnail_url)
