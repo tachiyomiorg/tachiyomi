0bcf54e53b0314f7c9ae77ed508287a65881fcc1
lmj0011 <9396189+lmj0011@users.noreply.github.com>
2020-08-04 06:11:10 -0500

    - find the next chapter in manga chapter list that can be downloaded when the next from current chapter is already downloaded

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/loader/ChapterLoader.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/loader/ChapterLoader.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi.ui.reader.loader
 
 import android.content.Context
 import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.data.database.DatabaseHelper
 import eu.kanade.tachiyomi.data.database.models.Manga
 import eu.kanade.tachiyomi.data.download.DownloadManager
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
@@ -9,6 +10,7 @@ import eu.kanade.tachiyomi.source.LocalSource
 import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.ui.reader.model.ReaderChapter
+import eu.kanade.tachiyomi.util.lang.launchIO
 import eu.kanade.tachiyomi.util.system.connectivityManager
 import rx.Completable
 import rx.Observable
@@ -27,7 +29,8 @@ class ChapterLoader(
     private val manga: Manga,
     private val source: Source
 ) {
-    val preferences: PreferencesHelper = Injekt.get()
+    private val preferences: PreferencesHelper = Injekt.get()
+    private val db: DatabaseHelper = Injekt.get()
 
     /**
      * Returns a completable that assigns the page loader and loads the its pages. It just
@@ -81,14 +84,33 @@ class ChapterLoader(
      */
     private fun getPageLoader(chapter: ReaderChapter, goingToNextChapter: Boolean = false): PageLoader {
         val isDownloaded = downloadManager.isChapterDownloaded(chapter.chapter, manga, true)
-        val shouldDownload = preferences.steadyChapterDownload() && !context.connectivityManager.isActiveNetworkMetered
+        val shouldDownload = preferences.steadyChapterDownload() && (!context.connectivityManager.isActiveNetworkMetered || !preferences.downloadOnlyOverWifi())
 
         return when {
-            isDownloaded -> DownloadPageLoader(chapter, manga, source, downloadManager)
+            isDownloaded -> {
+                /**
+                 * If this chapter is already downloaded then attempt to download the
+                 * next chapter ahead that is not.
+                 */
+                if(shouldDownload && goingToNextChapter) {
+                    launchIO {
+                        val dbChapters = db.getChapters(manga).executeAsBlocking().toMutableList()
+                        dbChapters.sortBy { it.chapter_number }
+
+                        val chapterToDl = dbChapters.firstOrNull { chp ->
+                            val downloaded = downloadManager.isChapterDownloaded(chp, manga, true)
+                            !downloaded && !chp.read && (chp.chapter_number > chapter.chapter.chapter_number)
+                        }
+
+                        chapterToDl?.let { downloadManager.downloadChapters(manga, listOf(it)) }
+                    }
+                }
+
+                DownloadPageLoader(chapter, manga, source, downloadManager)
+            }
 
             (shouldDownload && !isDownloaded && goingToNextChapter) -> {
                 downloadManager.downloadChapters(manga, listOf(chapter.chapter))
-                Timber.d("Downloading chapter -> ${chapter.chapter.name}")
                 DownloadPageLoader(chapter, manga, source, downloadManager)
             }
 
