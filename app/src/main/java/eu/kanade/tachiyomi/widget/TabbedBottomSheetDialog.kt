98fd358a06ededcaa4ef4f7be5a44291daff0d04
lmj0011 <9396189+lmj0011@users.noreply.github.com>
2020-08-30 01:24:12 -0500

    - show confirmation dialog when user selects "Set as Default" option in Chapter Settings

--- app/src/main/java/eu/kanade/tachiyomi/widget/TabbedBottomSheetDialog.kt
+++ app/src/main/java/eu/kanade/tachiyomi/widget/TabbedBottomSheetDialog.kt
@@ -1,16 +1,13 @@
 package eu.kanade.tachiyomi.widget
 
 import android.app.Activity
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.PopupMenu
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.database.models.Manga
 import eu.kanade.tachiyomi.databinding.CommonTabbedSheetBinding
-import eu.kanade.tachiyomi.util.chapter.ChapterSettingsHelper
-import eu.kanade.tachiyomi.util.system.toast
+import eu.kanade.tachiyomi.ui.manga.chapter.SetChapterSettingsDialog
 import eu.kanade.tachiyomi.util.view.popupMenu
 
 abstract class TabbedBottomSheetDialog(private val activity: Activity, private val manga: Manga? = null) : BottomSheetDialog(activity) {
@@ -26,31 +23,15 @@ abstract class TabbedBottomSheetDialog(private val activity: Activity, private v
         setContentView(binding.root)
     }
 
-    private lateinit var popupMenu: PopupMenu
-
     private fun showPopupMenu(view: View) {
-        popupMenu = view.popupMenu(
+        view.popupMenu(
             R.menu.default_chapter_filter,
             {
             },
             {
                 when (this.itemId) {
-                    R.id.apply_to_library -> {
-                        /**
-                         * keep menu open even after toggling checkbox
-                         * ref: https://stackoverflow.com/a/31727213/2445763
-                         */
-                        this.isChecked = !this.isChecked
-                        this.setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW)
-                        this.actionView = View(context)
-                        false
-                    }
                     R.id.save_as_default -> {
-                        ChapterSettingsHelper.setNewSettingDefaults(manga)
-                        if (popupMenu.menu.findItem(R.id.apply_to_library).isChecked) {
-                            ChapterSettingsHelper.updateAllMangasWithDefaultsFromPreferences()
-                        }
-                        context.toast(context.getString(R.string.chapter_settings_updated))
+                        manga?.let { SetChapterSettingsDialog(context, it).showDialog() }
                         true
                     }
                     else -> true
