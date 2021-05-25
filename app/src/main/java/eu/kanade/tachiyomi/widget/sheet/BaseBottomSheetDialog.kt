5ec014b93837e7b6facdce502693a3434b784411
Ivan Iskandar <iiiiskandar14@gmail.com>
2021-05-25 10:03:19 +0700

    Edge-to-edge bottom sheet when possible

--- app/src/main/java/eu/kanade/tachiyomi/widget/sheet/BaseBottomSheetDialog.kt
+++ app/src/main/java/eu/kanade/tachiyomi/widget/sheet/BaseBottomSheetDialog.kt
@@ -1,6 +1,9 @@
 package eu.kanade.tachiyomi.widget.sheet
 
 import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +11,10 @@ import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.data.preference.PreferenceValues
+import eu.kanade.tachiyomi.data.preference.PreferencesHelper
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 abstract class BaseBottomSheetDialog(context: Context) : BottomSheetDialog(context) {
 
@@ -21,12 +28,34 @@ abstract class BaseBottomSheetDialog(context: Context) : BottomSheetDialog(conte
         val rootView = createView(layoutInflater)
         setContentView(rootView)
 
-        sheetBehavior = BottomSheetBehavior.from(rootView.parent as ViewGroup)
+        val bottomSheet = rootView.parent as ViewGroup
+        sheetBehavior = BottomSheetBehavior.from(bottomSheet)
 
         // Enforce max width for tablets
         val width = context.resources.getDimensionPixelSize(R.dimen.bottom_sheet_width)
         if (width > 0) {
             sheetBehavior.maxWidth = width
+        }
+
+        // Set navbar color to transparent for edge-to-edge bottom sheet if we can use light navigation bar
+        // TODO Replace deprecated systemUiVisibility when material-components uses new API to modify status bar icons
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            window?.navigationBarColor = Color.TRANSPARENT
+            val isDarkMode = when (Injekt.get<PreferencesHelper>().themeMode().get()) {
+                PreferenceValues.ThemeMode.light -> false
+                PreferenceValues.ThemeMode.dark -> true
+                PreferenceValues.ThemeMode.system ->
+                    context.resources.configuration.uiMode and
+                        Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+            }
+            var flags = bottomSheet.systemUiVisibility
+            flags = if (isDarkMode) {
+                flags and View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
+            } else {
+                flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            }
+            bottomSheet.systemUiVisibility = flags
         }
     }
 }
