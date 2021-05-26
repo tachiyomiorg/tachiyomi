0a1f08c27ab5f07a37019f862e8fd710fbab9858
Ivan Iskandar <iiiiskandar14@gmail.com>
2021-05-26 18:02:53 +0700

    Set bottom sheet peek size to 50% screen height
    The current auto peek height gives too low value on landscape orientation

--- app/src/main/java/eu/kanade/tachiyomi/widget/sheet/BaseBottomSheetDialog.kt
+++ app/src/main/java/eu/kanade/tachiyomi/widget/sheet/BaseBottomSheetDialog.kt
@@ -5,6 +5,7 @@ import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialog
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.preference.PreferenceValues
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
+import eu.kanade.tachiyomi.util.system.displayCompat
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
@@ -29,6 +31,13 @@ abstract class BaseBottomSheetDialog(context: Context) : BottomSheetDialog(conte
         val width = context.resources.getDimensionPixelSize(R.dimen.bottom_sheet_width)
         if (width > 0) {
             behavior.maxWidth = width
+        }
+
+        // Set peek height to 50% display height
+        context.displayCompat?.let {
+            val metrics = DisplayMetrics()
+            it.getRealMetrics(metrics)
+            behavior.peekHeight = metrics.heightPixels / 2
         }
 
         // Set navbar color to transparent for edge-to-edge bottom sheet if we can use light navigation bar
