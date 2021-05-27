920032fe90bee78410414534b5c9ff1a86a7ee58
Soitora <simon.mattila@protonmail.com>
2021-05-27 05:48:38 +0200

    Animated icon for the Browse tab

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsMainController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsMainController.kt
@@ -53,7 +53,7 @@ class SettingsMainController : SettingsController() {
             onClick { navigateTo(SettingsTrackingController()) }
         }
         preference {
-            iconRes = R.drawable.ic_explore_outline_24dp
+            iconRes = R.drawable.ic_browse_outline_24dp
             iconTint = tintColor
             titleRes = R.string.browse
             onClick { navigateTo(SettingsBrowseController()) }
