974848daec89c88763fa127123ef4842b577a015
Andreas E <andreas.everos@gmail.com>
2021-06-16 11:27:24 +0200

    Add flags for sorting per category

--- app/src/main/java/eu/kanade/tachiyomi/data/database/models/Category.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/database/models/Category.kt
@@ -20,12 +20,35 @@ interface Category : Serializable {
         get() = flags and MASK
         set(mode) = setFlags(mode, MASK)
 
+    var sortMode: Int
+        get() = flags and SORT_MASK
+        set(mode) = setFlags(mode, SORT_MASK)
+
+    var sortDirection: Int
+        get() = flags and SORT_DIRECTION_MASK
+        set(mode) = setFlags(mode, SORT_DIRECTION_MASK)
+
     companion object {
 
         const val COMPACT_GRID = 0b00000000
         const val COMFORTABLE_GRID = 0b00000001
         const val LIST = 0b00000010
         const val MASK = 0b00000011
+
+        const val ALPHABETICAL = 0b00000000
+        const val LAST_READ = 0b00000100
+        const val LAST_CHECKED = 0b00001000
+        const val UNREAD = 0b00001100
+        const val TOTAL_CHAPTERS = 0b00010000
+        const val LATEST_CHAPTER = 0b00010100
+        const val DATE_FETCHED = 0b00011000
+        const val DATE_ADDED = 0b00011100
+        const val SORT_MASK = 0b00111100 // Mask supports for more sorting flags
+
+        const val ASCENDING = 0b00000000
+        const val DESCENDING = 0b01000000
+        const val SORT_DIRECTION_MASK = 0b01000000
+
 
         fun create(name: String): Category = CategoryImpl().apply {
             this.name = name
