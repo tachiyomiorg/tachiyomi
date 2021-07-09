1a23e2a1268d2674e9274ef81e7b31096e76abab
Ivan Iskandar <iiiiskandar14@gmail.com>
2021-07-09 19:20:22 +0700

    Convert quad-state multi choices dialogs

--- app/src/main/java/eu/kanade/tachiyomi/widget/materialdialogs/MaterialAlertDialogBuilderExtensions.kt
+++ app/src/main/java/eu/kanade/tachiyomi/widget/materialdialogs/MaterialAlertDialogBuilderExtensions.kt
@@ -5,7 +5,9 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import androidx.core.content.getSystemService
 import androidx.core.widget.doAfterTextChanged
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import eu.kanade.tachiyomi.databinding.DialogStubQuadstatemultichoiceBinding
 import eu.kanade.tachiyomi.databinding.DialogStubTextinputBinding
 
 fun MaterialAlertDialogBuilder.setTextInput(
@@ -26,4 +28,27 @@ fun MaterialAlertDialogBuilder.setTextInput(
         }
     }
     return setView(binding.root)
+}
+
+/**
+ * Sets a list of items with checkboxes that supports 4 states.
+ *
+ * @see eu.kanade.tachiyomi.widget.materialdialogs.QuadStateTextView
+ */
+fun MaterialAlertDialogBuilder.setQuadStateMultiChoiceItems(
+    items: List<CharSequence>,
+    initialSelected: IntArray,
+    disabledIndices: IntArray? = null,
+    selection: QuadStateMultiChoiceListener
+): MaterialAlertDialogBuilder {
+    val binding = DialogStubQuadstatemultichoiceBinding.inflate(LayoutInflater.from(context))
+    binding.list.layoutManager = LinearLayoutManager(context)
+    binding.list.adapter = QuadStateMultiChoiceDialogAdapter(
+        items = items,
+        disabledItems = disabledIndices,
+        initialSelected = initialSelected,
+        listener = selection
+    )
+    setView(binding.root)
+    return this
 }
